7057b1ec57c3dd6ece2a94e643a67a472cc61873
Francesco Sacchi <batt@develer.com>
2017-03-06 10:37:20 +0100

    Add commands.

--- scripts/develed.js
+++ scripts/develed.js
@@ -17,4 +17,43 @@ module.exports = function (robot) {
     res.reply('Ok ' + res.message.user.name + ', invio "' + msg + '" a DeveLED!');
   });
 
+  var frasi = [
+    'Che cosa accadrebbe se il male si formerebbe dentro di te?',
+    'Un virus micidiale che ti fa stare proprio una cosa malissimo...',
+    'Il peggio è tornato...',
+    'Esistono storie che non esistono.',
+    'Mio padre dice che diventerò un ottimo ballerina!',
+    "Io voglio fare l'usciere!",
+    "Ma tu sei Billy Ballo: il miglior ballerino del mondo!",
+    "Mannaggia, mannaggia, mannaggia...",
+    "Vedo la gente scema.. Tu sei il sesto, oggi!",
+    "Voglio divenire uno scienziato!",
+  ]
+
+  robot.respond(/pug bomb/i, function (res) {
+    res.reply(res.random(frasi));
+  });
+
+  robot.respond(/pug me/i, function (res) {
+    res.reply(res.random(frasi));
+  });
+
+  robot.respond(/ciao/i, function (res) {
+    res.reply(res.random(frasi));
+  });
+
+/*
+  robot.respond(/analizza (.*)/i, function (res) {
+    var url = res.match[1].trim()
+    robot.http("http://" + url)
+    	.get() function (err, res, body) {
+	    	if err
+	    		res.reply "Mi spiace, ma c'è stato un errore: #{err}"
+	    		return
+	    	
+	    	res.reply('Ok, ecco quello che ho preso:\n ' + body)
+	    }
+  });
+*/
+
 };
