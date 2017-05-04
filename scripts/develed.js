b4f14a62eb5e13219abfbb6dca25ba8c085b6076
Francesco Sacchi <batt@develer.com>
2017-05-04 11:51:42 +0200

    Add commands to write to the leds.

--- scripts/develed.js
+++ scripts/develed.js
@@ -11,18 +11,23 @@
 //   batt
 
 module.exports = function (robot) {
+  var room = 'C4AM1K414'
 
+  var fs = require('fs');
   var frasi_url = "https://dl.dropboxusercontent.com/s/nx7icory52foizv/lines.txt"
   var CronJob = require('cron').CronJob;
   var job = new CronJob('00 03 11,16 * * 1-5', function() {
     robot.http(frasi_url).get()(function(err, resp, body) {
         if (err) {
-          robot.messageRoom("embeddedisbetter", err);
+          robot.messageRoom(room, err);
         }
         else {
           var frasi = body.split('\n');
           var rnd = Math.floor(Math.random()*frasi.length);
-          robot.messageRoom("embeddedisbetter", frasi[rnd]);
+          robot.messageRoom(room, frasi[rnd]);
+          var writerStream = fs.createWriteStream('/home/root/intext');
+          writerStream.write("text=" +frasi[rnd]+"\n" + "font=font4x7\n");
+          writerStream.end();
         }
       });
     }, function () {
@@ -65,6 +70,61 @@ module.exports = function (robot) {
     "Voglio divenire uno scienziato!",
   ]
 
+  robot.respond(/write/i, function (res) {
+    var fs = require('fs');
+    fs.writeFile("/tmp/test.txt", "Hey there!\n", function (err) {
+      if(err) {
+          return console.log(err);
+      }
+      console.log("The file was saved!");
+    });
+    res.reply("scritto!");
+  });
+
+  robot.respond(/mostra (.*)/i, function (res) {
+    var fs = require('fs');
+     fs.readFile('/home/root/images/'+res.match[1].trim()+'.png', 'binary', function(err, original_data){
+     fs.writeFile('/home/root/images/'+res.match[1].trim()+'.png', original_data, 'binary', function(err) {});
+     var base64Image = new Buffer(original_data, 'binary').toString('base64');
+
+     var writerStream = fs.createWriteStream('/home/root/ledfifo');
+     writerStream.write(base64Image);
+     writerStream.end();
+     console.log("File streamed!");
+
+     fs.writeFile('/tmp/mostra.bin', base64Image, 'binary', function(err) {
+          console.log(err);
+     });
+    });
+    res.reply("Show image: " + res.match[1].trim());
+  });
+
+  robot.respond(/scrivi (.*)/i, function (res) {
+    var fs = require('fs');
+
+    var writerStream = fs.createWriteStream('/home/root/intext');
+    writerStream.write("text=" +res.match[1]+"\n");
+    writerStream.end();
+    console.log("File streamed!");
+    robot.messageRoom(room, res.message.user.name + ": " + res.match[1]);
+
+    res.reply("Write: " + res.match[1]);
+  });
+
+
+  robot.respond(/lista/i, function (res) {
+    const testFolder = '/home/root/images/';
+    const fs = require('fs');
+    var ss = ""
+    fs.readdir(testFolder, function (err, files) {
+      files.forEach(file, function(file) {
+        ss = ss + file + "\n"
+        console.log(file);
+      });
+    })
+    res.reply(res.random(frasi));
+  });
+
   robot.respond(/pug bomb/i, function (res) {
     res.reply(res.random(frasi));
   });
@@ -79,7 +139,7 @@ module.exports = function (robot) {
 
   robot.respond(/dici (.*)/i, function (res) {
     if (res.message.user.name == "batt") {
-      robot.messageRoom("embeddedisbetter", res.match[1].trim());
+      robot.messageRoom(room, res.match[1].trim());
     }
   });
 };
